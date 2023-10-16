2e1fc2ee4c65611049a7a3d98377054cd2bc3465
Atul Chandra <atulchandra021@gmail.com>
2023-10-16 20:49:47 +0530

    sortZeroOneTwo 2nd solution

--- Day2/sortZeroOneTwo.cpp
+++ Day2/sortZeroOneTwo.cpp
@@ -1,36 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void sortNumber(vector<int>& nums) {
-        int zero = 0, one = 0, two = 0;
-
+ void sortColors(vector<int>& nums) {
+        int ind = 0;
         for(int i=0;i<nums.size();i++){
             if(nums[i] == 0){
-                zero++;
-            }else if(nums[i] == 1){
-                one++;
-            }else{
-                two++;
+                swap(nums[i], nums[ind]);
+                ind++;
             }
         }
 
-        int i = 0;
-
-        while(zero--){
-            nums[i] = 0;
-            i++;
+        for(int i=ind;i<nums.size();i++){
+            if(nums[i] == 1){
+                swap(nums[i], nums[ind]);
+                ind++;
+            }
         }
-        while(one--){
-            nums[i] = 1;
-            i++;
-        }
-        while(two--){
-            nums[i] = 2;
-            i++;
-        }
-
-
-}
+    }
 
 int main(){
     int n;

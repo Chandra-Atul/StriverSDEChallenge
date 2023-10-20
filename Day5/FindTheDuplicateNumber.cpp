610a4317da08c7d5c064f3e0aec7950373743f2a
Atul Chandra <atulchandra021@gmail.com>
2023-10-20 15:35:59 +0530

    find dupliate number done 2nd sol

--- Day5/FindTheDuplicateNumber.cpp
+++ Day5/FindTheDuplicateNumber.cpp
@@ -1,3 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
@@ -6,6 +9,26 @@ public:
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i]==nums[i+1])  return nums[i];
         }
+        return -1;
+    }
+};
+
+
+
+/// 2nd solution
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int, int>mp;
+        for(auto it: nums)  mp[it]++;
+
+        for(auto it: mp){
+            if(it.second >= 2){
+                return it.first;
+            }
+        }
+
         return -1;
     }
 };
